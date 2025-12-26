bb37f6880aeae5ed2885752c85e4e6069d2c315a
Chris Fenner <commits@dlp.rip>
2025-12-25 20:32:42 -0800

    factor out rc typescript into own file

--- ts/index.ts
+++ ts/index.ts
@@ -1,46 +1,6 @@
-import { ReturnCodeLookupResult } from "../proto/rc";
-import { rcLookup } from "./rest";
+import { lookupReturnCode } from "./rc";
 
 import "./styles.css";
-
-// Ensure this function is globally accessible if using `onclick` in HTML
-export async function lookupReturnCode() {
-  // Get the input element and cast it to HTMLInputElement
-  const inputElement = document.getElementById(
-    "rc-query-input"
-  ) as HTMLInputElement;
-
-  const resultsElement = document.getElementById(
-    "rc-results"
-  ) as HTMLDivElement;
-
-  const inputValue: string = inputElement.value;
-
-  // All TPM error codes are 3-digit hexadecimal numbers.
-  if (inputValue.length != 3) {
-    resultsElement.textContent = "";
-    return;
-  }
-
-  try {
-    const rsp = await rcLookup({
-      query: inputValue,
-    });
-
-    resultsElement.textContent = "";
-
-    rsp.result.forEach((res: ReturnCodeLookupResult) => {
-      const newDiv = document.createElement("div");
-      newDiv.setAttribute("class", "rc-result");
-      newDiv.setAttribute("box-", "square");
-      newDiv.textContent = res.description;
-      resultsElement.appendChild(newDiv);
-    });
-  } catch (error) {
-    console.error("Error:", error);
-    alert("Failed to post data.");
-  }
-}
 
 document.addEventListener("DOMContentLoaded", (event) => {
   const queryInput = document.getElementById(
